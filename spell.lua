3bf2f0010aa4ccfc61907c5f2860e75bfa94e6cf
Florian <florian.bernier@soait.fr>
2023-02-19 19:47:24 +0100

    spell base

--- spell.lua
+++ spell.lua
@@ -1,48 +1,99 @@
 local Spell = {}
-local listeSpell = {}
 
-local CONST = {
-    type = {fire = "fire", midnight = "midnight"},
-    fire = {},
-    midnight = {}
+Spell.anim = {
 }
+Spell.anim.life = love.graphics.newImage("images/spell/fire.png")
+Spell.anim.fire = love.graphics.newImage("images/spell/midnight.png")
 
-Spell.fire = love.graphics.newImage("images/spell/fire.png")
-Spell.midnight = love.graphics.newImage("images/spell/midnight.png")
 
-function CreateSpell(pX1, pY1, pX2, pY2, pSpell)
-    local newSpell = {x1 = pX1, y1 = pY1, x2 = pX2, y2 = pY2, spell = pSpell}
-    table.insert(listeSpell, newSpell)
-end
+Spell.anim.currentQuad = 1
+Spell.anim.currentTime = 0
+Spell.anim.frameDuration = 0.05
+
+Spell.anim.showAnim = false
+Spell.anim.showAnim2 = false
+Spell.anim.x = 0
+Spell.anim.y = 0
 
 --- --- --- --- --- ---
 
 local function loadQuad()
-    CONST.fire.quad = {}
+    local quads = {} -- Tableau pour stocker tous les quads
     for i = 0, 7 do -- Boucle à travers chaque ligne de quads
         for j = 0, 7 do -- Boucle à travers chaque colonne de quads
-            CONST.fire.quad = love.graphics.newQuad(j * 100, i * 100, 100, 100, Spell.fire:getDimensions())
+            local quad = love.graphics.newQuad(j * 100, i * 100, 100, 100, Spell.anim.life:getDimensions())
+            table.insert(quads, quad) -- Ajouter le quad actuel à la table
         end
     end
+    for i = 0, 7 do -- Boucle à travers chaque ligne de quads
+        for j = 0, 7 do -- Boucle à travers chaque colonne de quads
+            local quad = love.graphics.newQuad(j * 100, i * 100, 100, 100, Spell.anim.fire:getDimensions())
+            table.insert(quads, quad) -- Ajouter le quad actuel à la table
+        end
+    end
+    Spell.anim.quads = quads -- Stocker tous les quads dans l'objet Spell
 end
-
-
 
 Spell.load = function()
     loadQuad()
 end
 
+
+local function updateSpellAnim(dt)
+    if Spell.anim.showAnim or Spell.anim.showAnim2  then
+        Spell.anim.currentTime = Spell.anim.currentTime + dt
+        if Spell.anim.currentTime >= Spell.anim.frameDuration then
+            Spell.anim.currentTime = Spell.anim.currentTime - Spell.anim.frameDuration
+            Spell.anim.currentQuad = Spell.anim.currentQuad + 1
+            if Spell.anim.currentQuad > 64-3 then
+                Spell.anim.currentQuad = 1
+                --table.remove(Spell.anim.quads, Spell.anim.currentTime)
+                Spell.anim.showAnim = false
+            end
+        end
+        -- Déplacer le sort vers la position de la souris
+        local mx, my = love.mouse.getPosition()
+        local dx = mx - Spell.anim.x
+        local dy = my - Spell.anim.y
+        local angle = math.atan2(dy, dx)
+        local speed = 100
+        if Spell.anim.fire then
+            Spell.anim.x = Spell.anim.x + math.cos(angle) * speed * dt
+            Spell.anim.y = Spell.anim.y + math.sin(angle) * speed * dt
+        end
+    end
+end
+
+
 Spell.update = function(dt)
+    updateSpellAnim(dt)
 end
 
 
 Spell.draw = function()
+    if Spell.anim.showAnim then
+        local currentQuad = Spell.anim.quads[Spell.anim.currentQuad]
+        love.graphics.draw(Spell.anim.life, currentQuad, Spell.anim.x+Camera_x, Spell.anim.y+Camera_y)
+    end
+    if Spell.anim.showAnim2 then
+        local currentQuad = Spell.anim.quads[Spell.anim.currentQuad]
+        love.graphics.draw(Spell.anim.fire, currentQuad, Spell.anim.x+Camera_x, Spell.anim.y+Camera_y)
+    end
 end
 
 
 Spell.keypressed = function(key)
     if key == "e" then
-        CreateSpell(0, 0, 100, 100, CONST.fire.quad)
+        Spell.anim.showAnim = true
+        Spell.anim.currentTime = 0
+        Spell.anim.currentQuad = 1
+        --Spell.anim.x, Spell.anim.y = love.mouse.getPosition()
+    end
+    if key == "a" then
+        Spell.anim.showAnim2 = true
+        Spell.anim.currentTime = 0
+        Spell.anim.currentQuad = 1
+        --Spell.anim.x, Spell.anim.y = love.mouse.getPosition()
     end
 end
 
